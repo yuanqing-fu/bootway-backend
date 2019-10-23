248859fbbcfa821a189711b80a3c0569a71d19bc
kofbossyagami <kofbossyagami@163.com>
2019-10-23 20:39:00 +0800

    Creating tasks.

--- routes/tasks.js
+++ routes/tasks.js
@@ -10,9 +10,8 @@ module.exports = (router, db, auth, config) => {
   //         "Authorization": "Bearer %s" % your_token
   //     }).json()
 
+  //  获得当前用户任务列表
   router.get('/', async (ctx, next) => {
-    //todo 用户id是放在参数还是header里
-    //get task list
     ctx = auth(ctx, config)
 
     if (ctx.body.type !== 'error') {
@@ -22,12 +21,24 @@ module.exports = (router, db, auth, config) => {
 
   // 创建任务
   router.post('/', async (ctx, next) => {
+    ctx = auth(ctx, config)
+    if (ctx.body.type !== 'error') {
+      const name = ctx.request.body.name
+      const important = ctx.request.body.important
+      const urgent = ctx.request.body.urgent
+      const start_date = ctx.request.body.start_date
 
+      console.log(name,important,urgent,new Date(start_date))
+      ctx.request.body.start_date = new Date(start_date)
+      ctx.request.body.user_id = ctx.body.result.id
+      ctx.request.body.done = false
+      ctx.body = await db('task').insert(ctx.request.body)
+    }
   })
 
   // 删除任务
   router.del('/:task_id', async (ctx, next) => {
-
+    ctx = auth(ctx, config)
   })
 
   // router.get("/:id", auth, async (ctx, next) => {
