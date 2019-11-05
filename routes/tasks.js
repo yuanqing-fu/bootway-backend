3c15b4185691254cdd65c23abbb877f0f25415cf
kofbossyagami <kofbossyagami@163.com>
2019-11-05 14:51:09 +0800

    Task edit feature.

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,7 +6,9 @@ module.exports = (router, db, auth, moment, config) => {
 
     if (ctx.body.type !== 'error') {
       let startDate = new Date(ctx.params.start_date)
+      startDate.setHours(0, 0, 0)
       let endDate = moment(startDate).add(1, 'day').toDate()
+      endDate.setHours(0, 0, 0)
       ctx.body = await db('task').where({
         'user_id': ctx.body.result.id
       }).andWhereBetween('start_date', [startDate, endDate])
@@ -17,16 +19,33 @@ module.exports = (router, db, auth, moment, config) => {
   router.post('/', async (ctx, next) => {
     ctx = auth(ctx, config)
     if (ctx.body.type !== 'error') {
-      const name = ctx.request.body.name
+      ctx.request.body.start_date = new Date(ctx.request.body.start_date)
+      ctx.request.body.user_id = ctx.body.result.id
+      ctx.request.body.done = false
+      ctx.body = await db('task').insert(ctx.request.body)
+    }
+  })
+
+  // 编辑任务
+  router.put('/', async (ctx, next) => {
+    ctx = auth(ctx, config)
+    if (ctx.body.type !== 'error') {
+      const task_id = ctx.request.body.task_id
+      const task_name = ctx.request.body.task_name
+      const done = ctx.request.body.done
       const important = ctx.request.body.important
       const urgent = ctx.request.body.urgent
       const start_date = ctx.request.body.start_date
 
-      console.log(name, important, urgent, new Date(start_date))
-      ctx.request.body.start_date = new Date(start_date)
-      ctx.request.body.user_id = ctx.body.result.id
-      ctx.request.body.done = false
-      ctx.body = await db('task').insert(ctx.request.body)
+      ctx.request.body.start_date = new Date(ctx.request.body.start_date)
+
+      ctx.body = await db('task').where({
+        'task_id': ctx.request.body.task_id
+      }).update(ctx.request.body)
+      // ctx.request.body.start_date = new Date(start_date)
+      // ctx.request.body.user_id = ctx.body.result.id
+      // ctx.request.body.done = false
+      // ctx.body = await db('task').insert(ctx.request.body)
     }
   })
 
